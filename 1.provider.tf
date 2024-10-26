5f8999b98b19b4db720b1c73d90bd30096871e9e
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-10-26 11:38:31 +0530

    addeded provider

---
+++ 1.provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "1.9.8"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.7.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.73.0"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+
+  }
+}
+
+provider "azurerm" {
+   features {}
+}
+provider "aws" {}
+provider "google" {}
