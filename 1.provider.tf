75700e23a9c226794f5f7c8d75197d2f80f53414
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-10-26 15:07:06 +0530

    added subnet-4

--- 1.provider.tf
+++ 1.provider.tf
@@ -15,10 +15,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "COMMON_RG"  
-    storage_account_name = "devsecopsdevtfstate"                      
-    container_name       = "tfstate"                       
-    key                  = "terraform.tfstate"        
+    resource_group_name  = "COMMON_RG"
+    storage_account_name = "devsecopsdevtfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
--- 5.subnets.tf
+++ 5.subnets.tf
@@ -21,6 +21,13 @@ resource "azurerm_subnet" "subnet-3" {
   address_prefixes     = ["10.42.3.0/24"]
   depends_on           = [azurerm_subnet.subnet-2] #explicit dependency
 }
+resource "azurerm_subnet" "subnet-4" {
+  name                 = "subnet-4"
+  resource_group_name  = azurerm_resource_group.devsecops-rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.42.4.0/24"]
+  depends_on           = [azurerm_subnet.subnet-3] #explicit dependency
+}
 
 # explicit = will not depend on other resource for creating
 # if we need to add dependency we can use = depends_on [resource_subnet.subnet2]
