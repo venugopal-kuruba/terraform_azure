5110045f1c5d56dab26ffca14195e8805ced0b1e
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-10-26 13:53:57 +0530

    add vnet and subnets depends on implicit and Explicit

---
+++ 4..virtual_network.tf
@@ -0,0 +1,16 @@
+resource "azurerm_virtual_network" "vnet1" {
+  name = "vnet1"
+  #implicit dependency on Resource group and resource name 
+  location            = azurerm_resource_group.devsecops-rg1.location
+  resource_group_name = azurerm_resource_group.devsecops-rg1.name
+  address_space       = ["10.42.0.0/16"]
+
+  tags = {
+    env = "Development"
+  }
+}
+
+
+# implicit = depends on other resource
+# explicit = will not depend on other resource for creating
+# if we need to add dependency we can use = depends_on [resource_subnets.subnet3]
---
+++ 5.subnets.tf
@@ -0,0 +1,26 @@
+resource "azurerm_subnet" "subnet-1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.devsecops-rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.42.1.0/24"]
+
+}
+
+resource "azurerm_subnet" "subnet-2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.devsecops-rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.42.2.0/24"]
+  depends_on           = [azurerm_subnet.subnet-1] #explicit dependency
+}
+
+resource "azurerm_subnet" "subnet-3" {
+  name                 = "subnet-3"
+  resource_group_name  = azurerm_resource_group.devsecops-rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.42.3.0/24"]
+  depends_on           = [azurerm_subnet.subnet-2] #explicit dependency
+}
+
+# explicit = will not depend on other resource for creating
+# if we need to add dependency we can use = depends_on [resource_subnet.subnet2]
