136ac525d1036591d937bb35c023d945ef26c495
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-10-27 13:42:36 +0530

    changes in terraform wrapper to cahnged subnet

--- deploy_terraform.sh
+++ deploy_terraform.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 CB=$(git branch --show-current)
-if [ "${CB}"="dev" ]; then
+if [ "${CB}"=="dev" ]; then
     echo "running terraform on branch ${CB}.."
     rm -rf .terraform
     rm -rf 1.provider-prod.tf
     terraform init
     terraform fmt && terraform validate
     terraform plan
-else
+    terraform apply --auto-approve
+elif [ "${CB}"=="master" ]; then
     echo "running terraform on branch ${CB}.."
     rm -rf .terraform
     rm -rf 0.provider-dev.tf
     terraform init
     terraform fmt && terraform validate
     terraform plan
+    terraform apply --auto-approve
+else
+    echo "running terraform on branch ${CB} which is not Dev or master."
+    echo "So, not running any terraform...!"
 fi
